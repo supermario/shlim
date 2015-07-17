c0529cb2d994071c7d8efb0822c9421b3b7eeac3
Mario Rogic <hello@mario.net.au>
2015-07-17 22:07:49 +1000

    #id support and refactor

--- src/Shlim.hs
+++ src/Shlim.hs
@@ -10,16 +10,14 @@ parseLines :: String -> String
 parseLines = unlines . map (parseLine . words) . lines
 
 parseLine :: [String] -> String
-parseLine (x:xs) = concat ["<", open, ">", unwords xs, "</", close, ">"]
-  where
-      parts = tagParts $ x
-      (close:_) = parts
-      open = concat . (map tag) $ parts
+parseLine (x:xs) = concat ["<", unwords $ el:attr, ">", unwords xs, "</", el, ">"]
+  where (el:attr) = (map tag) $ tagParts $ x
 
 tagParts :: String -> [String]
-tagParts = split (keepDelimsL $ oneOf ".")
+tagParts = split (keepDelimsL $ oneOf ".#")
 
 tag :: String -> String
-tag ('.':xs) = " class='" ++ xs ++ "'"
+tag ('.':xs) = "class='" ++ xs ++ "'"
+tag ('#':xs) = "id='"    ++ xs ++ "'"
 tag ""       = "div"
 tag x        = x
--- test/ShlimSpec.hs
+++ test/ShlimSpec.hs
@@ -7,7 +7,13 @@ spec :: Spec
 spec = do
   describe "parseLine" $ do
     it "wraps tags" $ do
-      parseLine ["div", "content"] `shouldBe` "<div>content</div>"
+      parseLine ["div", "content"]       `shouldBe` "<div>content</div>"
 
-    it "wraps tags" $ do
-      parseLine ["div.test", "content"] `shouldBe` "<div class='test'>content</div>"
+    it "handles .classes" $ do
+      parseLine [".test", "content"]     `shouldBe` "<div class='test'>content</div>"
+
+    it "handles #ids" $ do
+      parseLine ["#test", "content"]     `shouldBe` "<div id='test'>content</div>"
+
+    it "handles #ids.classes" $ do
+      parseLine ["#test.ing", "content"] `shouldBe` "<div id='test' class='ing'>content</div>"
